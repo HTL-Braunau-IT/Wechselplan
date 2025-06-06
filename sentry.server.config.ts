bff2decc63e0f07284c61dd7f15f7b676a5b13b7
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2025-06-06 10:39:44 +0200

    Https -> http

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -3,12 +3,20 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 Sentry.init({
   dsn: "http://fbcb57caebb7f5faf9c1bc3e4b11de3d@sentry.htl-braunau.at/1",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
+
+  // Set sampling rate for profiling - this is evaluated only once per SDK.init call
+  profilesSampleRate: 1.0,
+
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
